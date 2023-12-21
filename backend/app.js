30d6873e6d7e88312743c2ece80b352f2ad13692
shazzzank <shanky.zrk@gmail.com>
2023-12-21 23:07:15 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -33,9 +33,9 @@ app.use('/api/v1', order);
 app.use('/api/v1', payment);
 
 if(process.env.NODE_ENV === 'PRODUCTION'){
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  app.use(express.static(path.join(__dirname, './frontend/build')));
+  app.use('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
   });
 } 
 app.use(errorMiddleware);
