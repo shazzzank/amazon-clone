097bb98afb42db3c72be9e2803906e9af210f1f0
shazzzank <shanky.zrk@gmail.com>
2023-12-21 23:54:20 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDatabase = () =>{
 	mongoose.connect(process.env.DB_URI).then(conn=> console.log(`MongoDB is connected with HOST: ${conn.connection.host}`));
